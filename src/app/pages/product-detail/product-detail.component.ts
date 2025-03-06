38f4531d5b659990ddab899edb2c3ca5b23a367b
JhonGta <jhonguaman-10@hotmail.com>
2025-03-06 09:34:59 -0500

    nav

--- src/app/pages/product-detail/product-detail.component.ts
+++ src/app/pages/product-detail/product-detail.component.ts
@@ -3,10 +3,11 @@ import { Car } from '../../models/car';
 import { ActivatedRoute } from '@angular/router';
 import { CarService } from '../../services/car.service';
 import { CommonModule } from '@angular/common';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
 
 @Component({
   selector: 'app-product-detail',
-  imports: [CommonModule],
+  imports: [CommonModule, NavbarComponent],
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.css'
 })
